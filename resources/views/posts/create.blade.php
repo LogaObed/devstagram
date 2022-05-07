68495cb59a79ff6529b923f3c0d3e54c6bc076a9
Obed Loeza <obedloga97@gmail.com>
2022-05-06 22:49:20 -0500

    Formulario publicaicones memnu y funciona sin dropzone

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app')
 @section('titulo')
-    Create Post
-    
+    Crea una nueva Publicación
+@endsection
+@section('contenido')
+    <div class="md:flex md:items-center">
+        <div class="md:w-6/12 px-10 bg-purple-400">
+            <p>imagen</p>
+        </div>
+        <div class="md:w-5/12 p-10 bg-white rounded-lg shadow-xl">
+            <form class="w-full max-w-sm" action="{{ route('registar') }}" method="post" novalidate>
+
+                @csrf
+                <div class="mb-5">
+                    <label for="titulo" class="mb-2 capitalize text-gray-500 font-bold">titulo</label>
+                    <input value="{{ old('titulo') }}" type="text" placeholder="Titulo de la Publicación..." name="titulo" id="titulo"
+                        class="mt-1 capitalize border p-2.5 w-full rounded-lg shadow-sm focus:outline-none focus:bg-white focus:border-purple-500 @error('titulo') border-red-600 @enderror">
+                    @error('titulo')
+                        <p class="bg-red-500 text-white my-2 rounded-sm text-sm p-2 text-center normal-case">{{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="descripcion" class="mb-2 capitalize text-gray-500 font-bold">Descripción</label>
+                    <textarea name="descripcion" placeholder="Descripcion de la Publicación..."  id="descripcion" class="mt-1 capitalize border p-2.5 w-full rounded-lg shadow-sm focus:outline-none focus:bg-white focus:border-purple-500 @error('descripcion') border-red-600 @enderror" >{{ old('descripcion') }}</textarea>
+                        @error('descripcion')
+                        <p class="bg-red-500 text-white my-2 rounded-sm text-sm p-2 text-center normal-case">{{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <input type="submit" value="Crear Publicación"
+                class="shadow bg-purple-500 hover:bg-purple-400 mt-2 transition-colors cursor-pointer capitalize font-bold w-full p-2.5 text-white rounded-lg">
+
+            </form>
+        </div>
+    </div>
 @endsection
